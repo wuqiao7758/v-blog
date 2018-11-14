dab692559c47bf309938cb9a618d7c05b729373e
wuqiao7758 <https://github.com/wuqiao7758/v-blog.git>
2018-11-14 11:23:50 +0800

    登录完毕...开始权限判断

--- server/Controller/UserController.js
+++ server/Controller/UserController.js
@@ -6,13 +6,42 @@ export default{
 } */
 // 这是以前的写法，通常我们都会把处理过程写成函数，直接暴露函数即可。
 // 以后我们采用ES6 class类
+import User from '../models/UserModel'
+import md5 from 'md5'
+import createToken from '../utils/createToken'
 class UserController{
     // 登录的处理逻辑在这里
     async login(ctx){
+        let user = ctx.request.body.user
+        let password = ctx.request.body.password
     // 验证用户名/密码是否合法
-        
+        console.log(user);
+        console.log(password);
     // 检查用户是否存在
-
+const res = (await User.getUserByName(user))[0];
+console.log(res);
+if(res){
+    // 如果存在
+    // 检查密码是否正确
+    if(res.password === md5(password)){
+        let token = createToken(res.user)
+        ctx.body = {
+            success:true,
+            message:'登录成功....',
+            token:token
+        }
+    }else{
+        ctx.body ={
+            seccess:false,
+            message:'密码错误,请重新填写...'
+        }
+    }
+}else{
+    ctx.body={
+        success:false,
+        message:'用户名不存在，请重新填写...',
+    }
+}
     // 检查密码是否正确
 
     // 生成token
---
+++ server/models/UserModel.js
@@ -0,0 +1,9 @@
+import query from '../utils/query'
+
+class UserModel {
+    async getUserByName(user){
+        return await query(`SELECT * FROM user WHERE user='${user}'`)
+    }
+}
+
+export default new UserModel()
